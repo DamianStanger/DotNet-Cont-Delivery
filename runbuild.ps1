aadd922f77f1a46bc6ce7799dd4eef6c04246110
Damian Stanger <damian.stanger@gmail.com>
2012-02-01 13:16:04 +0000

    initial commit

---
+++ runbuild.ps1
@@ -0,0 +1,11 @@
+# Local Variables
+$MsBuild = $env:systemroot + "\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe";
+$SlnFilePath = "ContinuousDelivery.sln"
+ 
+$BuildArgs = @{
+ FilePath = $MsBuild
+ ArgumentList = $SlnFilePath, "/target:Clean", "/target:Build"
+}
+ 
+# Start the build
+Start-Process @BuildArgs
