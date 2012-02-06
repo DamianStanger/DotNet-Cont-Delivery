eed9255b172f55942264507f1ced788200b3bb0e
Damian Stanger <damian.stanger@gmail.com>
2012-02-06 13:32:54 +0000

    build file now uses 32 bit powershell

--- runbuild.ps1
+++ runbuild.ps1
@@ -2,7 +2,7 @@
 
 
 # Local Variables
-$MsBuild = $env:systemroot + "\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe";
+$MsBuild = $env:systemroot + "\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe";
 $SlnFilePath = "ContinuousDelivery.sln"
  
 $BuildArgs = @{
