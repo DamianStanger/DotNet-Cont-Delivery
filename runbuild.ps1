32ce8a46a83cfe93852b04186876fb9f675f50d8
Damian Stanger <damian.stanger@gmail.com>
2012-02-06 13:16:20 +0000

    build file

--- runbuild.ps1
+++ runbuild.ps1
@@ -1,3 +1,6 @@
+# run this first -   Set-ExecutionPolicy RemoteSigned
+
+
 # Local Variables
 $MsBuild = $env:systemroot + "\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe";
 $SlnFilePath = "ContinuousDelivery.sln"
