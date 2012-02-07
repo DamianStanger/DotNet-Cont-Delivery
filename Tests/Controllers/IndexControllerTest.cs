32ed14772b8af7243fdaa7554a3288c4997883bb
Damian Stanger <damian.stanger@gmail.com>
2012-02-07 13:36:24 +0000

    Added tests to solution

---
+++ Tests/Controllers/IndexControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using ContinuousDelivery.Controllers;
+
+namespace Tests.Controllers
+{
+    [TestFixture]
+    public class IndexControllerTest
+    {
+        [Test]
+        public void ShouldReturnTrue()
+        {
+            var ic = new IndexController();
+
+            var view = ic.Index();
+
+            Assert.True(true);
+        }
+
+        [Test]
+        public void ShouldFail()
+        {
+            var ic = new IndexController();
+
+            var view = ic.Index();
+
+            Assert.True(false);
+        }
+    }
+}
