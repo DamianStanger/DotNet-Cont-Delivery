5b5cae918a398998cc757c72922993835764cf52
Damian Stanger <damian.stanger@gmail.com>
2012-02-10 22:27:44 +0000

    rename assest zip file to continuousDelivery

--- 7zip.ps1
+++ 7zip.ps1
@@ -1,2 +1,2 @@
 mkdir assets
-libs\7zip\7z.exe a -tzip assets\archive.zip ContinuousDelivery\build\_PublishedWebsites\ContinuousDelivery
+libs\7zip\7z.exe a -tzip assets\ContinuousDelivery.zip ContinuousDelivery\build\_PublishedWebsites\ContinuousDelivery
