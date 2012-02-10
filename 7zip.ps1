7dc0b96ca46d42b487fb20d0a6e052eae6fbcd76
Damian Stanger <damian.stanger@gmail.com>
2012-02-10 23:04:26 +0000

    fixed 7zip command so paths are not put in the zip file

--- 7zip.ps1
+++ 7zip.ps1
@@ -1,2 +1,4 @@
 mkdir assets
-libs\7zip\7z.exe a -tzip assets\ContinuousDelivery.zip ContinuousDelivery\build\_PublishedWebsites\ContinuousDelivery
+cd ContinuousDelivery\build\_PublishedWebsites\ContinuousDelivery
+..\..\..\..\libs\7zip\7z.exe a -tzip -r ..\..\..\..\assets\ContinuousDelivery.zip *
+cd ..\..\..\..
