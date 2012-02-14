561d17b5c88f6dd06ea56bf4e42af01979aa1501
Damian Stanger <damian.stanger@gmail.com>
2012-02-14 12:54:55 +0000

    changes to scripts to deliver content to iis website

--- installToIIS.ps1
+++ installToIIS.ps1
@@ -1,19 +1,24 @@
+#Set-StrictMode -Version 2
+
 # run this first -   Set-ExecutionPolicy RemoteSigned
 
 import-module WebAdministration
 
-Remove-Item IIS:\Sites\ContinuousDelivery -recurse
-Remove-Item IIS:\AppPools\ContinuousDeliveryAppPool -recurse
-Remove-Item website -recurse
+Remove-Item IIS:\Sites\ContinuousDelivery -recurse -Force
+Remove-Item IIS:\AppPools\ContinuousDeliveryAppPool -recurse -Force
+Remove-Item website -recurse -Force
 
 New-Item website -type Directory
 
 libs\7zip\7z.exe x assets\ContinuousDelivery.zip -oWebsite
 
 New-Item IIS:\AppPools\ContinuousDeliveryAppPool
-New-Item iis:\Sites\ContinuousDelivery -bindings @{protocol="http";bindingInformation=":8080:ContinuousDelivery"} -physicalPath C:\_projects\visualStudio\ContinuousDelivery\website
+Set-ItemProperty IIS:\AppPools\ContinuousDeliveryAppPool -Name managedRuntimeVersion -Value "v4.0"
+New-Item iis:\Sites\ContinuousDelivery -bindings @{protocol="http";bindingInformation=":80:ContinuousDelivery"} -physicalPath C:\_projects\visualStudio\ContinuousDelivery\website
 Set-ItemProperty IIS:\Sites\ContinuousDelivery -name applicationPool -value ContinuousDeliveryAppPool
 
-
+Start-Sleep -s 2
 $webclient = New-Object Net.WebClient
-$webclient.DownloadString("http://continuousdelivery:8080/");
+$webclient.DownloadString("http://continuousdelivery")
+
+#$result = $webclient.DownloadString("http://continuousdelivery").IndexOf('<meta name="ContinuousDelivery" />')
--- runbuild.ps1
+++ runbuild.ps1
@@ -1,15 +1,17 @@
+Set-StrictMode -Version 2
+
 # run this first -   Set-ExecutionPolicy RemoteSigned
 # Start-Process @BuildArgs -NoNewWindow
 
 
 # Local Variables
 $MsBuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe";
-$SlnFilePath = "ContinuousDelivery.sln"
+$SlnFilePath = "ContinuousDelivery.sln";
  
 $BuildArgs = @{
- FilePath = $MsBuild
- ArgumentList = $SlnFilePath, "/target:Clean", "/target:Build", "/property:OutDir=build\"
+ FilePath = $MsBuild;
+ ArgumentList = $SlnFilePath, "/target:Clean", "/target:Build", "/property:OutDir=build\";
 }
  
 # Start the build
-Start-Process @BuildArgs
+Start-Process @BuildArgs;
